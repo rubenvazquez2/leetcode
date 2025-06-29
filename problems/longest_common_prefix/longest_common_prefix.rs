9bd725ac3a3b75eef8eeab423bc974d74ca56855
Ruben Vazquez Jr. <rubenvazquez.2@outlook.com>
2025-06-29 16:31:15 -0400

    Update formatting for longest_common_prefix solution.

--- problems/longest_common_prefix/longest_common_prefix.rs
+++ problems/longest_common_prefix/longest_common_prefix.rs
@@ -1,6 +1,4 @@
-
-struct Solution {
-}
+struct Solution {}
 
 impl Solution {
     pub fn longest_common_prefix(strs: Vec<String>) -> String {
@@ -11,20 +9,16 @@ impl Solution {
             for elem in &strs {
                 let possible_next_letter = elem.chars().nth(i);
                 match current_letter {
-                    None => {
-                        match possible_next_letter {
-                            None => break 'index_loop,
-                            Some(letter) => current_letter = Some(letter),
-                        }
+                    None => match possible_next_letter {
+                        None => break 'index_loop,
+                        Some(letter) => current_letter = Some(letter),
                     },
-                    Some(letter) => {
-                        match possible_next_letter {
-                            None => break 'index_loop,
-                            Some(next_letter) => {
-                                if next_letter != letter {
-                                    break 'index_loop;
-                                }
-                            },
+                    Some(letter) => match possible_next_letter {
+                        None => break 'index_loop,
+                        Some(next_letter) => {
+                            if next_letter != letter {
+                                break 'index_loop;
+                            }
                         }
                     },
                 }
@@ -40,8 +34,16 @@ impl Solution {
 }
 
 fn main() {
-    let strs = vec!(String::from("flower"), String::from("flow"), String::from("flight"));
+    let strs = vec![
+        String::from("flower"),
+        String::from("flow"),
+        String::from("flight"),
+    ];
     println!("{}", Solution::longest_common_prefix(strs));
-    let strs = vec!(String::from("dog"), String::from("racecar"), String::from("car"));
+    let strs = vec![
+        String::from("dog"),
+        String::from("racecar"),
+        String::from("car"),
+    ];
     println!("{}", Solution::longest_common_prefix(strs));
 }
