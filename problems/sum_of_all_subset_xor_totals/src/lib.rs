0655ec3b60f8263895ede42aac2fcf17e5bb559f
Ruben Vazquez Jr. <rubenvazquez.2@outlook.com>
2025-07-02 14:39:16 -0400

    Add Rust solution for subset_of_all_subset_xor_totals.

--- problems/sum_of_all_subset_xor_totals/src/lib.rs
+++ problems/sum_of_all_subset_xor_totals/src/lib.rs
@@ -2,7 +2,24 @@ pub struct Solution {}
 
 impl Solution {
     pub fn subset_xor_sum(nums: Vec<i32>) -> i32 {
-        -1
+        Solution::subset_xor_sum_recurse(nums, 0)
+    }
+
+    pub fn subset_xor_sum_recurse(nums: Vec<i32>, idx: usize) -> i32 {
+        if nums.len() == 0 {
+            0
+        } else if idx == nums.len() {
+            let mut answer = 0;
+            for num in nums {
+                answer = answer ^ num;
+            }
+            answer
+        } else {
+            let mut nums_clone = nums.clone();
+            nums_clone.remove(idx);
+            Solution::subset_xor_sum_recurse(nums, idx + 1)
+                + Solution::subset_xor_sum_recurse(nums_clone, idx)
+        }
     }
 }
 
